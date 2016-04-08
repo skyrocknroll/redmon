08fe9c396e96cd1c515a2b20e19b7cfd83130ddf
SkyRocknRoll <uvaraj6@gmail.com>
2016-04-08 14:20:14 +0530

    Initial working version commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # redmon
-Redis Monitoring 
+Redis & Sentinel  Monitoring
+By default the server listens on endpoint `0.0.0.0:6379`
 
-This tool exposes an http://<ip>:9736/status api which you can add it to any of the http endpoint monitoring tool. 
+## Example
+
+`http://localhost:9736/redis/<redis endpoint>`
+### Example url to monitor redis which is running in localhost:6379
+Ex:
+`http://localhost:9736/redis/localhost:6379`
+
+`http://localhost:9736/sentinel/<comma sepeareted sentinel enpoints>/<MaterName>`
+
+### Example url to monitor redis using sentinel failover
+Sentinel Hosts
+* localhost:26379
+* localhost:26378
+
+Ex
+`http://localhost:9736/sentinel/localhost:26379,localhost:26378/master`
+
+
---
+++ monitor.go
@@ -0,0 +1,88 @@
+//author SkyRocknRoll
+
+package main
+
+import (
+	"gopkg.in/redis.v3"
+	"fmt"
+	"net/http"
+	"io"
+	"github.com/gorilla/mux"
+	"strings"
+	"log"
+	"flag"
+)
+
+func main() {
+	ListenEndpoint := flag.String("listen-enpoint", "0.0.0.0:9736", "Enpoint to listen ex: 0.0.0.0:9736")
+	r := mux.NewRouter()
+	r.HandleFunc("/redis/{endpoint}", RedisStatus)
+	r.HandleFunc("/sentinel/{endpoint}/{master}", RedisSentinelStatus)
+
+	fmt.Printf("Server listening on %s", *ListenEndpoint)
+	err := http.ListenAndServe(*ListenEndpoint, r)
+	if err != nil {
+		fmt.Println(err.Error())
+		log.Fatal("Unable to start the server!!!")
+	}
+
+}
+
+// Creates New Redis Client
+func GetNewClient(Endpoint string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     Endpoint,
+		Password: "", // no password set
+		DB:       0, // use default DB
+	})
+
+	return client
+}
+
+//Provides new redis client using sentinel
+func GetNewFailoverClient(MasterName string, SentinelAddrs []string) *redis.Client {
+	client := redis.NewFailoverClient(&redis.FailoverOptions{
+		MasterName:    MasterName,
+		SentinelAddrs: SentinelAddrs,
+		Password: "", // no password set
+		DB:       0, // use default DB
+	})
+
+	return client
+}
+
+func RedisStatus(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	client := GetNewClient(vars["endpoint"])
+	defer client.Close()   // Close the client.
+	data, err := client.Set("__||__", "Are You Up?", -1).Result()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		//fmt.Println(err.Error())
+
+	} else {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, data)
+
+	}
+}
+
+func RedisSentinelStatus(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	SentinelAddrs := strings.Split(vars["endpoint"], ",")
+	MasterName := vars["master"]
+	client := GetNewFailoverClient(MasterName, SentinelAddrs)
+	defer client.Close()   // Close the client.
+	data, err := client.Set("__||__", "Are You Up?", -1).Result()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		//fmt.Println(err.Error())
+
+	} else {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, data)
+
+	}
+}
